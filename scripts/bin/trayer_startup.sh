1f6931d4008f236876c74f45975be18d4fbbe15a
defunsm <salmanhossain500@gmail.com>
2022-11-28 01:45:53 -0500

    add: ws-manager settings file

---
+++ scripts/bin/trayer_startup.sh
@@ -0,0 +1,1 @@
+trayer --edge bottom --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 2 --transparent true --alpha 0 --tint 0x282c34  --height 22 &
--- scripts/bin/ws-manager.sh
+++ scripts/bin/ws-manager.sh
@@ -2,9 +2,27 @@
 
 export GUM_INPUT_PROMPT_FOREGROUND="#0FF"
 
+# location of where settings is stored
+SETTING_FILE_PATH="$HOME/bin/ws-manager.settings"
+
+# create the settings file if it doesn't exist
+if [[ ! -e $SETTING_FILE_PATH ]]; then
+  touch $SETTING_FILE_PATH
+fi
+
+# displays a nice green border when executing ws-manager
 gum style \
 	--foreground "#40EDB6" --border-foreground "#40EDB6" --border double \
 	--align center --width 50 --margin "1 2" --padding "2 4" \
 	'Workspace Manager'
 
-gum choose $(ls -d .*/ */)
+# adds,removes or cd to the project
+CHOICE=$(gum choose "add project folder" "remove project folder" "go to project")
+echo $CHOICE
+# appends the pwd of the project into settings file
+if [[$CHOICE -eq "add project folder"]]; then
+  CURRENT_PWD=$(pwd)
+  echo $CURRENT_PWD > $SETTING_FILE_PATH
+fi
+
+
